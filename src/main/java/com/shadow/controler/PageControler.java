c25f162c87fbbb5cf2b4a589605aac0101eade92
yq654321 <410337453@qq.com>
2018-01-22 10:42:13 +0800

    11

--- src/main/java/com/shadow/controler/PageControler.java
+++ src/main/java/com/shadow/controler/PageControler.java
@@ -8,12 +8,46 @@ public class PageControler {
 
     @RequestMapping("index")
     public String index(){
-        return "bill_handout";
+        return "index";
     }
 
+
+//单据管理
+    @RequestMapping("billhandout")
+    public  String billhandout(){
+
+
+       return "billhandout";
+    }
+//权限管理
+    @RequestMapping("authorization")
+    public  String   authorization(){
+
+
+        return "authorization";
+    }
+    //数据字典
     @RequestMapping("Config_datadict")
     public String Config_datadict(){
         return "Config_datadict";
     }
 
+
+//用户管理
+    @RequestMapping("User")
+    public String User(){
+        return "User";
+    }
+
+    //角色管理
+    @RequestMapping("role")
+    public String role(){
+        return "role";
+    }
+
+    //菜单管理
+    @RequestMapping("configmenu")
+    public String configmenu(){
+        return "configmenu";
+    }
 }
